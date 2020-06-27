d04ecb2db288efb05916798959589a739e5c1801
xuchuan <1164009771@qq.com>
2020-06-27 15:33:45 +0800

    完成了登录和注册功能的开发啊

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,10 @@
-// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
-  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+// 注意：每次调用$.get()或$.post()或$.ajax()的时候，会先
+// 调用ajaxPrefilter这个函数
+// 在这个函数中，可以拿到我们给ajax提供的配置对象
+// options：(Object对象)当前AJAX请求的所有参数选项。
+$.ajaxPrefilter(function(options){
+  //在发起真正ajax请求之前，统一拼接请求的根路径
+  console.log(options.url);
+  options.url = "http://ajax.frontend.itheima.net"+options.url;
+  console.log(options.url)
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,80 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
-  })
+$(function () {
+  // 点击去注册的连接
+  $("#link_reg").on("click", function () {
+    $(".login-box").hide();
+    $(".reg-box").show();
+  });
 
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
-  })
 
-  // 从 layui 中获取 form 对象
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+  // 点击去登陆的链接
+  $("#link_login").on("click", function () {
+    $(".login-box").show();
+    $(".reg-box").hide();
+  });
+  // 从layui中获取form对象
+  var form=layui.form;
+  var layer=layui.layer;
+  // 通过verify()函数自定义校验规则
   form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
+    // 自定义一个叫pwd的校验规则
+    pwd:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
+    // 定义一个验证pwd的校验规则
+    repwd:function(value){
+      // 通过形参拿到确认密码框中的内容
+      // 还需要拿到密码框的内容
       // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
-        return '两次密码不一致！'
+      // 如果判断失败，则return一个提示信息
+      var pwd=$('.reg-box [name=password]').val();
+      if(pwd !==value){
+        return '两次密码不一致'
       }
     }
   })
 
   // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
+  $('#form_reg').on('submit',function(e){
+    // 阻止默认行为
     e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
-    })
-  })
-
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
-    e.preventDefault()
+    // 发起ajax请求
     $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('登录失败！')
+      url:'/api/reguser',
+      data:{
+        username:$('#form_reg [name=username]').val(),
+        password:$('#form_reg [name=password]').val()
+      },
+      type:'post',
+      dataType:'json',
+      success:function(res){
+        if(res.status !==0){
+          return layer.msg(res.message)
+          
         }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
+        layer.msg('注册成功，请登录')
+        // 模拟人的点击事件
+        $('#link_login').click()
       }
     })
   })
-})
+  // 监听登录表单的提交事件
+  $('#form_login').submit(function(e){
+    // 阻止默认行为
+    e.preventDefault();
+    $.ajax({
+      url: "/api/login",
+      data:$(this).serialize(),
+      type:'post',
+      dataType:'json',
+      success:function(res){
+        if(res.status !==0){
+          return layer.msg('登录失败')
+        }
+        layer.msg('登录成功');
+        
+        // 将登录成功得到的token字符串保存到localStorage中
+        localStorage.setItem('token',res.token)
+        // 跳转到后台主页
+        location.href='./index.html'
+      }
+    });
+  })
+});
